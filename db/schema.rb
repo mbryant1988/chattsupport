7da83c543c600115956b2bdc221a7a2138b26861
Michael Bryant <mikeakakbe@gmail.com>
2018-02-20 19:42:47 -0500

    config for postgres/heroku

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20180219204904) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "businesses", force: :cascade do |t|
     t.string "company"
     t.text "description"
